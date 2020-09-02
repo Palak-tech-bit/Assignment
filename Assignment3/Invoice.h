46911cba8a5564e8339bba23f7a38713fd32abfc
Palak-tech-bit <68230919+Palak-tech-bit@users.noreply.github.com>
2020-09-02 14:36:08 +0530

    Add files via upload

---
+++ Assignment3/Invoice.h
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+class Invoice
+{
+    private:
+
+    string partno;
+    string partde;
+    int quantity;
+    int price;
+
+    public:
+    Invoice(string str,string ,int ,int );
+
+
+    void setPartno(string );
+
+    void setPartde(string );
+
+     void setquantity(int );
+
+     void setPrice(int );
+
+    string getPartno();
+
+    string getPartde();
+
+     int getQuantity();
+
+     int getPrice();
+
+    int getInvoiceAmount();
+
+
+};
+
+
+
+
